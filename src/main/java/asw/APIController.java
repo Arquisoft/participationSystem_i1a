a18408e8f3b38b555b8cfeb865d4bf41c2aaa7dc
Pabloski_gl <pythonwonders@gmail.com>
2017-04-05 02:12:57 +0200

    Solved URL mapping collision
    For the sake of simplicity, REST petitions will be accepted in URLs
    starting by REST.

--- src/main/java/asw/APIController.java
+++ src/main/java/asw/APIController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class APIController {
 
-    @RequestMapping("/user")
+    @RequestMapping("/REST/user")
     public UserInfo user() {
         return new UserInfo("pepe",0);
     }
